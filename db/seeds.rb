20592d7dd8f7f3fade528244af85901128716460
morgan krischer <morgan.krischer@gmail.com>
2018-05-16 15:39:23 +0100

    Form new review done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+restaurants_attributes = [
+{name: "Little Viet Kitchen", address: "Tottenham Court Road", category: "vietnamese", phone_number: "01 23 45 67 89"},
+{name: "Untitled", address: "Kingsland Road", category: "japanese", phone_number: "01 23 45 67 89"},
+{name: "Lilane", address: "Place Monge", category: "french", phone_number: "01 23 45 67 89"},
+{name: "Belgo", address: "London", category: "belgian", phone_number: "01 23 45 67 89"},
+{name: "Zizzi", address: "Oxford Circus", category: "italian", phone_number: "01 23 45 67 89"}
+]
+
+Restaurant.create!(restaurants_attributes)
